f30aa36b3b06c29a970fd8fd965d62c72d7e4feb
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-01-05 11:13:31 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,12 @@
+// for loop
+for (var i = 0; i < 5; i++) {
+    console.log("Iteration " + i);
+  }
+  
+  // while loop
+  var count = 0;
+  while (count < 5) {
+    console.log("Count is " + count);
+    count++;
+  }
+  
