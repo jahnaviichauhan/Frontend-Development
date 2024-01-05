f42565bbcc652916e72e51c71abb41bcbbc7bc92
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-01-05 11:13:08 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,9 @@
+var temperature = 25;
+
+if (temperature > 30) {
+  console.log("It's hot outside!");
+} else if (temperature >= 20) {
+  console.log("It's a pleasant day.");
+} else {
+  console.log("It's cold outside.");
+}
